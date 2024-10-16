de0072fc4f960882a33a6468453e03ae6e7dda48
hridoykit <hridoy.tsa@gmail.com>
2024-10-16 14:52:40 +0600

    change home screen to playgound by click + added utility func

---
+++ src/main.js
@@ -0,0 +1,25 @@
+function play(){
+    hideElement('home');
+    showElement('playground');
+    console.log('first')
+}
+// document.getElementById('play-btn').addEventListener('click',function(){
+//     console.log('play button clicked');
+//     document.getElementById('home').classList.add('hidden');
+//     document.getElementById('playground').classList.remove('hidden');
+// });
+
+// function play(){
+//     document.getElementById('home').classList.add('hidden');
+//     document.getElementById('playground').classList.remove('hidden');
+//     console.log('first')
+// }
+
+// const alphabet = document.getElementById('alphabet');
+// const randomApha = Math.round(Math.random() * 25);
+// console.log(randomApha)
+// document.getElementById('random-alphabet').innerText = randomApha;
+
+// document.getElementById('alphabet').addEventListener('click', function(e){
+//     console.log(e.target.innerText)
+// });
---
+++ src/utility.js
@@ -0,0 +1,8 @@
+// hide home section
+function hideElement(id){
+    document.getElementById(id).classList.add('hidden');
+}
+
+function showElement(id){
+    document.getElementById(id).classList.remove('hidden');
+}
